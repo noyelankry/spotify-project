e8c9b4bec1137373d51f259df7d37a466ad83284
Noy Elankry <noy.elankry@gmail.com>
2022-07-04 18:04:14 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'spot-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'spotify-project';
+}
